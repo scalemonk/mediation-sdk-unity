137c08907b8aaa37baec1c4aea219f7d9a92865f
Axel Savizky <axel.savizky@wildlifestudios.com>
2021-03-23 16:08:28 -0300

    changed portrait design and added event triggers

--- Assets/ScaleMonk Ads/Source/AdsEditorBinding.cs
+++ Assets/ScaleMonk Ads/Source/AdsEditorBinding.cs
@@ -12,9 +12,11 @@ namespace ScaleMonk.Ads
     public class AdsEditorBinding : IAdsBinding
     {
         private MockAd _mockAdInstance;
+        private ScaleMonkAds _scaleMonkAds;
 
         public void Initialize(ScaleMonkAds adsInstance)
         {
+            _scaleMonkAds = adsInstance;
             Debug.Log("ScaleMonkAds initialized successfully");
 #if UNITY_2018_4_OR_NEWER
             MockAd mockAdPrefab;
@@ -39,15 +41,18 @@ namespace ScaleMonk.Ads
             _mockAdInstance.gameObject.SetActive(true);
             _mockAdInstance.SetText("AN INTERSTITIAL AD WILL BE DISPLAYED HERE");
 #endif
+            _scaleMonkAds.CompletedInterstitialDisplay(tag);
         }
 
         public void ShowRewarded(string tag)
         {
+            _scaleMonkAds.StartedRewardedDisplay(tag);
             Debug.Log("Rewarded shown at " + tag);
 #if UNITY_2018_4_OR_NEWER
             _mockAdInstance.gameObject.SetActive(true);
             _mockAdInstance.SetText("A REWARDED AD WILL BE DISPLAYED HERE");
 #endif
+            _scaleMonkAds.CompletedRewardedDisplay(tag);
         }
 
         public bool IsInterstitialReadyToShow(string analyticsLocation)
