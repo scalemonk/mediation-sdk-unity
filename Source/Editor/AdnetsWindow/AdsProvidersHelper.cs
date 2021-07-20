e0f9306060866267be5abad8f7ba16759ed92510
Nicolás Ravasi <nicolas.ravasi@wildlifestudios.com>
2021-07-20 17:27:21 -0300

    update ios to 2.2.0 (#67)

--- Source/Editor/AdnetsWindow/AdsProvidersHelper.cs
+++ Source/Editor/AdnetsWindow/AdsProvidersHelper.cs
@@ -16,7 +16,7 @@ namespace ScaleMonk.Ads
 {
     public class AdsProvidersHelper
     {
-        const string iosAdsVersion = "2.1.0";
+        const string iosAdsVersion = "2.2.0";
         const string androidAdsVersion = "4.1.0";
 
         public static string GetAdnetsXmlPath()
