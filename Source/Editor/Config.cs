536be9c1885bd482ed9f40ab3f7cd383341cbd54
Nicolas Ravasi <nicolas.ravasi@wildlifestudios.com>
2021-08-19 15:40:00 -0300

    Release 1.13.0

--- Source/Editor/Config.cs
+++ Source/Editor/Config.cs
@@ -2,6 +2,6 @@ namespace ScaleMonk.Ads
 {
     public static class Config
     {
-        public static readonly string Version = "1.13.0-rc.1";
+        public static readonly string Version = "1.13.0";
     }
 }
