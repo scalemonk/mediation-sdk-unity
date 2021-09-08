5e71825173079032d2789e01376dbfbc25b4116a
Jonatan Nielavitzky <jonatan.nielavitzky@wildlifestudios.com>
2021-09-08 16:28:10 -0300

    RC 1.17.0-rc.1 with iOS RC 3.3.0-rc.1

--- Source/Editor/AdnetsWindow/AdsProvidersHelper.cs
+++ Source/Editor/AdnetsWindow/AdsProvidersHelper.cs
@@ -16,7 +16,7 @@ namespace ScaleMonk.Ads
 {
     public class AdsProvidersHelper
     {
-        const string iosAdsVersion = "3.2.0";
+        const string iosAdsVersion = "3.3.0-rc.1";
         const string androidAdsVersion = "6.2.0";
       
         public static string GetAdnetsXmlPath()
