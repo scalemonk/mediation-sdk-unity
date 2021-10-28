e70e7599cb25bbc758c569c1faadfa61b2aa8de4
Federico Maleh <fedemaleh@yahoo.com.ar>
2021-10-28 17:01:04 -0300

    Update iOS SDK to 1.21.0 (#99)
    Co-authored-by: Federico Maleh <federico.maleh@wildlifestudios.com>

--- Source/Editor/Config.cs
+++ Source/Editor/Config.cs
@@ -2,6 +2,6 @@ namespace ScaleMonk.Ads
 {
     public static class Config
     {
-        public static readonly string Version = "1.20.0";
+        public static readonly string Version = "1.21.0";
     }
 }
