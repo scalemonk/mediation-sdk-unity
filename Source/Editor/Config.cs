675626ab4feccd8182262a04096e2fa2ad9d9b5e
Ariel Felipe Camacho Diaz <ariel.camacho@wildlifestudios.com>
2021-11-09 15:31:16 -0300

    Augment release version

--- Source/Editor/Config.cs
+++ Source/Editor/Config.cs
@@ -2,6 +2,6 @@ namespace ScaleMonk.Ads
 {
     public static class Config
     {
-        public static readonly string Version = "1.21.0";
+        public static readonly string Version = "1.22.0";
     }
 }
